90f32260e6baa89d01c444395f979002d973f205
nnlangdo <nksproday@gmail.com>
2022-05-15 12:44:50 +0530

    on15may2022

--- blog_app/views.py
+++ blog_app/views.py
@@ -34,7 +34,8 @@ class HomeView(ListView):
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-',' '))
-    return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
+    return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 
+    'category_posts':category_posts})
 
 class ArticelDetailView(DeleteView):
     model = Post
